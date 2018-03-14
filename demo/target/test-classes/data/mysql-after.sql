c44f6eb2bdc8e815c5e0884e7cf7feb83d901ee0
arrizaqu <arrizaqu@yahoo.com>
2018-03-14 17:23:41 +0700

    sp

---
+++ demo/target/test-classes/data/mysql-after.sql
@@ -0,0 +1,6 @@
+insert into employee(name, address) values ("masyda arrizaqu", "seputih banyak");
+insert into employee(name, address) values ("sri wahyuni", "jakarta timur");
+insert into employee(name, address) values ("didit gunadhi", "tangerang");
+insert into employee(name, address) values ("amel", "lampung");
+insert into employee(name, address) values ("yusuf", "bogor");
+-- 
